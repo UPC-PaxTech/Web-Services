d9553afb8d7fe8950234bbecceaaef8d48fbc47e
Suin Yum <u202210838@upc.edu.pe>
2025-06-03 17:21:48 -0500

    feat: AccountCommandServiceImplementation

---
+++ src/main/java/com/paxtech/utime/platform/profiles/application/internal/commandservices/AccountCommandServiceImpl.java
@@ -0,0 +1,29 @@
+package com.paxtech.utime.platform.profiles.application.internal.commandservices;
+
+import com.paxtech.utime.platform.profiles.domain.model.aggregates.Account;
+import com.paxtech.utime.platform.profiles.domain.model.commands.CreateAccountCommand;
+import com.paxtech.utime.platform.profiles.domain.services.AccountCommandService;
+import com.paxtech.utime.platform.profiles.infrastructure.persistence.jpa.AccountRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AccountCommandServiceImpl implements AccountCommandService {
+    private final AccountRepository accountRepository;
+
+    public AccountCommandServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public Optional<Account> handle(CreateAccountCommand command) {
+        if (accountRepository.existsByUsername(command.userName())){
+            throw new IllegalArgumentException("Account with same username already exists");
+        }
+
+        var account = new Account(command);
+        var createdAccount = accountRepository.save(account);
+        return Optional.of(createdAccount);
+    }
+}
