b41502967ae5375499e560f10ab2a3239866cae8
Gael Rs <gaelrszk@gmail.com>
2025-06-14 19:07:16 -0500

    feat: add aggregates, commands and queries aboun iam bounded

---
+++ src/main/java/com/paxtech/utime/platform/iam/domain/model/aggregates/User.java
@@ -0,0 +1,42 @@
+﻿package com.paxtech.utime.platform.iam.domain.model.aggregates;
+
+import com.paxtech.utime.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * User aggregate root
+ * This class represents the aggregate root for the User entity.
+ *
+ * @see AuditableAbstractAggregateRoot
+ */
+
+@Getter
+@Setter
+@Entity
+public class User extends AuditableAbstractAggregateRoot<User> {
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    public User() {}
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+}
